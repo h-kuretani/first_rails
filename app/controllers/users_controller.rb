e580862a83171ee8ffaf8ac85b150a9e7c420cbf
Yoshito Kamizato <yuyueureka157@gmail.com>
2018-08-02 22:02:47 +0900

    add create function to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,15 @@ class UsersController < ApplicationController
 	end
 
 	def create
-
+		# O/Rマッパー（Object Relational マッピング）
+		# オブジェクトのデータをデータベースに登録できる
+		# 裏側で勝手にSQLを発行してくれる
+		# user = User.new(name: params[:user][:name], age: params[:user][:age], hobby: params[:user][:hobby])
+		# user.save
+		# if User.create(name: params[:user][:name], age: params[:user][:age], hobby: params[:user][:hobby])
+		# 	puts "保存成功"
+		# end
+		puts "保存成功" if User.create(name: params[:user][:name], age: params[:user][:age], hobby: params[:user][:hobby])
 	end
 
 	def update
