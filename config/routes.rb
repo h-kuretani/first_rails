73f18d93184d1069bb52a81c56d27c8fbfa578ea
呉谷　秀幸 <>
2018-09-03 18:53:09 +0900

    add resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root to:'users#index'
 
-  get '/users/index', to:'users#index'
-  get '/users/new', to:'users#new'
-  get '/users/edit', to:'users#edit'
-  get '/users/show', to:'users#show'
-  post '/users', 	 to:'users#create'
-  get '/users/update', to:'users#update'
-  get '/users/destroy', to:'users#destroy'
+  # get '/users/index', to:'users#index'
+  # get '/users/new', to:'users#new'
+  # get '/users/edit', to:'users#edit'
+  # get '/users/show', to:'users#show'
+  # post '/users', 	 to:'users#create'
+  # get '/users/update', to:'users#update'
+  # get '/users/destroy', to:'users#destroy'
 
-
+resources :users
 end
 
